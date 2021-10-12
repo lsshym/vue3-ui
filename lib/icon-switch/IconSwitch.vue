7c1193df1cc69e810a9b099e0ddfe24495404328
qiuyan <qiuyanli@faceunity.com>
2021-10-12 14:07:32 +0800

    ✨

---
+++ lib/icon-switch/IconSwitch.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="icon-switch" @click="handleIconClick" :style="style">
+        <span
+            v-show="!modelValue"
+            :style="{
+                background: `url('${icon}') center
+                center/cover no-repeat`,
+            }"
+        >
+        </span>
+        <span
+            v-show="modelValue"
+            :style="{
+                background: `url('${activeIcon}') center
+                center/cover no-repeat`,
+            }"
+        >
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, CSSProperties, PropType } from "vue";
+export default defineComponent({
+    name: "",
+    props: {
+        modelValue: Boolean,
+        icon: String,
+        activeIcon: String,
+        style: Object as PropType<CSSProperties>,
+    },
+    methods: {
+        handleIconClick(evt: MouseEvent) {
+            evt.stopImmediatePropagation();
+            this.$emit("click", !this.modelValue);
+            this.$emit("update:modelValue", !this.modelValue);
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.icon-switch {
+    display: inline-block;
+    & > span {
+        display: inline-block;
+        width: 36px;
+        height: 36px;
+    }
+}
+</style>
---
+++ note/webpack相关.md
@@ -0,0 +1,3 @@
+
+## chainWebpack
+chainWebpack感觉是自己搞得一套什么东西，我还是优先用`configureWebpack`
